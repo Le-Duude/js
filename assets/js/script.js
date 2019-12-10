f6b3f6291924d84cbc64c375cfdbcf126f5a2713
Le-Duude <57392459+Le-Duude@users.noreply.github.com>
2019-12-10 12:53:49 +0100

    function année bisectil si ça s'écrit comme ça

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,40 @@
 
 // document.write(message);
 
-let message = '<ul>';
 
-for(let i = -1722; i < 2756 ; i++) {
-  if (i % 9 === 0) {
-      message += '<li>';
-      message += i;
+// afficher multiple 9
+// let message = '<ul>';
+//
+// for(let i = -1722; i < 2756 ; i++) {
+//   if (i % 9 === 0) {
+//       message += '<li>';
+//       message += i;
+//
+//       message += '</li>';
+//   }
+// }
+//
+// message += '</ul>';
+// console.log('wololo');
+// document.write(message);
 
-      message += '</li>';
-  }
-}
 
-message += '</ul>';
-alert('wololo');
-document.write(message);
+//
+// let valeur =  prompt('Veuillez saisir un nom')
+// let msg = '';
+//
+// console.log(valeur);
+//
+// if (!isNaN(valeur)) {
+//
+//     if(valeur > 0) { msg = 'Positif'; }
+//     else if(valeur <0) { msg ='Nefatif'; }
+//     else { msg ='Nul !'}
+//
+// }
+//
+// else msg = 'Un nombre j\'ai dit';
+// document.write(msg);
+
+
+document.getElementById( elementId 'truc').innerText = 'coucou';
